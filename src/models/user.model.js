24209ee26d81d8770b4258929945143c81c38780
Ali Monoar <amonwar45@gmail.com>
2026-04-06 01:32:57 +0600

    basic tamplate and fix mongo db connection

---
+++ src/models/user.model.js
@@ -0,0 +1,12 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+  },
+});
