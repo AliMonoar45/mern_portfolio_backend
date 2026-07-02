e48d7a1890c26e82804af145e7c56c15504dba40
Ali Monoar <amonwar45@gmail.com>
2026-07-02 10:57:58 +0600

    add comment router complete

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+import commentControllers from "../controllers/comment.controller.js";
+import { validateUser } from "../middleware/validation.middleware.js";
+
+const router = express.Router();
+
+// create comment
+router.post("/create-comment", validateUser, commentControllers.createComment);
+// get all comment
+router.get("/all-comment", validateUser, commentControllers.allComment);
+// get single comment
+router.get(
+  "/single-comment/:id",
+  validateUser,
+  commentControllers.singleComment,
+);
+// update comment
+router.put(
+  "/update-comment/:id",
+  validateUser,
+  commentControllers.updateComment,
+);
+// delete comment
+router.delete(
+  "/delete-comment/:id",
+  validateUser,
+  commentControllers.deleteComment,
+);
+
+export default router;
