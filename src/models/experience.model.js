58eb0bea19c246eedd721b53da1f21c632efb707
Ali Monoar <amonwar45@gmail.com>
2026-07-07 22:44:52 +0600

    simplify experience model

--- src/models/experience.model.js
+++ src/models/experience.model.js
@@ -1,33 +1,20 @@
-import { model, Schema } from "mongoose";
-const experienceSchema = new Schema(
+import mongoose from "mongoose";
+
+const experienceSchema = new mongoose.Schema(
   {
     title: {
       type: String,
-      required: true,
-      maxlength: 200,
-      trim: true,
     },
-    company: {
-      type: String,
-      required: true,
-      maxlength: 100,
-      trim: true,
-    },
-    time: {
-      type: Number,
-      required: true,
-      min: 0,
-    },
-    description: {
-      type: String,
-      trim: true,
-      maxlength: 1000,
-    },
+    company: { type: String },
+    description: { type: String },
+    time: { type: String },
   },
   {
     timestamps: true,
     versionKey: false,
   },
 );
-const Experience = model("Experience", experienceSchema);
+
+const Experience = mongoose.model("Experience", experienceSchema);
+
 export default Experience;
